3728254aa65a08d00f9a2d606af852f543167b34
DAftav <108040952+DAftav@users.noreply.github.com>
2025-04-18 11:12:05 +0530

    Add files via upload

---
+++ plot.py
@@ -0,0 +1,12 @@
+#Task-1
+x=int(input("Enter the 1st value: "))
+y=int(input("Enter the 2nd value: "))
+print("Addition:", x+y)
+print("Subtraction:", x-y)
+print("Multiplication:", x*y)
+print("Division:", x/y)
+
+#Task-2
+x=str(input("Enter the 1st name: "))
+y=str(input("Enter the 2nd name: "))
+print("Hello, " +  x  +  y  +  "!"  + " wellcome to the python program ")
