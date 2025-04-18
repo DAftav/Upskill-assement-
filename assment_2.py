9a2c9697324b5b8949de4d652127b78ae38b0ab1
DAftav <108040952+DAftav@users.noreply.github.com>
2025-04-18 15:10:10 +0530

    Add files via upload

---
+++ assment_2.py
@@ -0,0 +1,14 @@
+#task 1
+x=int(input("Enter the value:"))
+
+if (x%2==0):
+    print(x, ":is an Even number")
+else:
+    print(x, ":is a Odd number")
+
+# task 2
+
+for i in range (1,51):
+    y = (i*(i+1))/2
+print("The sum of numbers from 1 to 50 is:", y)
+    
